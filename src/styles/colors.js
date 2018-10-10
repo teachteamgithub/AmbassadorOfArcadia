2c0de406d4e63507a5125f080b46defea2a4ef39
Gleydson Rodrigues <gleydsonsr@gmail.com>
2018-10-10 15:24:00 -0300

    update styles

---
+++ src/styles/colors.js
@@ -0,0 +1,16 @@
+export default {
+    primary: '#8F6291',
+    darker: '#111',
+    dark: '#333',
+    regular: '#666',
+    light: '#C0C0C0',
+    lighter: '#eee',
+    white: '#FEFEFF',
+    transparent: 'rgba(0, 0, 0, 0)',
+};
+
+//#27102A
+//#210C33 - Blackcurrant
+//#8F6291 - Violet Blue
+//#C481F8 - Heliotrope
+//#FEFEFF - Ghost White
