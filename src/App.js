c2af57e80a456eb7b2fdc96f8ecbd2f4d08128bd
Gleydson Rodrigues <gleydsonsr@gmail.com>
2018-10-15 18:10:09 -0300

    added redux Provider

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 import Routes from './Routes';
+import reducers from './reducers';
 
 export default class App extends Component {
     render() {
         return (
-            <Routes />
+            <Provider store={createStore(reducers)}>
+                <Routes />
+            </Provider>
         );
     }
 }
