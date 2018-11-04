35643a3b42a7ab7b0f32726341d01b60fe1bdc0b
Gleydson Rodrigues <gleydsonsr@gmail.com>
2018-11-04 15:25:32 -0300

    ModalBox finalized

--- src/components/ModalBox/index.js
+++ src/components/ModalBox/index.js
@@ -4,10 +4,12 @@ import {
     View,
     Text,
     Alert,
-    Image
+    Image,
+    TouchableWithoutFeedback
 } from 'react-native';
 import PropTypes from 'prop-types';
 
+import ButtonWithImage from '../ButtonWithImage';
 import styles from './styles';
 
 const BUTTON_CLOSE = require('../../assets/images/buttons/close.png');
@@ -16,51 +18,96 @@ export default class ModalBox extends PureComponent {
     constructor(props) {
         super(props);
         this.state = {
-            modalVisible: true
+            modalVisible: false
         }
     }
 
+    setModalVisible() {
+        this.setState({ modalVisible: !this.state.modalVisible });
+    }
 
-    setModalVisible(visible) {
-        this.setState({ modalVisible: visible });
+    sizeContent(size) {
+        switch (size) {
+            case 'small':
+                return styles.smallFont;
+            case 'regular':
+                return styles.regularFont;
+            case 'big':
+                return styles.bigFont;
+            default:
+                return styles.regularFont;
+        }
     }
 
     render() {
         const {
-
+            title,
+            hasCloseButton,
+            content,
+            sizeContent,
+            okButton,
+            sizeOkButton,
+            textOkButton,
+            modalAction,
+            visible
         } = this.props;
         return (
-            <View
-                style={styles.container}
-            >
-                <Modal
-                    animationType="slide"
-                    transparent
-                    visible={this.state.modalVisible}
-                    onRequestClose={() => {
-                        Alert.alert('Modal has been closed.');
-                    }}
+            <View>
+                {visible ? this.setModalVisible : undefined}
+                <View
+                    style={styles.container}
                 >
-                    <View style={styles.containerModal}>
-                        <View style={styles.closeButtonContainer}>
-                            <Image source={BUTTON_CLOSE} style={styles.closeButton} />
+                    <Modal
+                        animationType="slide"
+                        transparent
+                        visible={this.state.modalVisible}
+                        onRequestClose={() => {
+                            Alert.alert('Modal has been closed.');
+                        }}
+                    >
+                        <View style={styles.containerModal}>
+                            <View style={styles.closeButtonContainer}>
+                                {title ? <View style={styles.containerTitle}>
+                                    <Text style={styles.title}>{title}</Text>
+                                </View> : undefined}
+                                <View style={styles.containerCloseButton}>
+                                    {hasCloseButton ?
+                                        <TouchableWithoutFeedback onPress={() => this.setModalVisible()}>
+                                            <Image source={BUTTON_CLOSE} style={styles.closeButton} />
+                                        </TouchableWithoutFeedback>
+                                        : undefined}
+                                </View>
+                            </View>
+                            {content ?
+                                <View style={styles.contentContainer}>
+                                    <Text style={{ ...styles.textContent, ...this.sizeContent(sizeContent) }}>
+                                        {content}
+                                    </Text>
+                                    {okButton ?
+                                        <View style={styles.okButton}>
+                                            <TouchableWithoutFeedback>
+                                                <ButtonWithImage
+                                                    size={sizeOkButton}
+                                                    text={textOkButton}
+                                                    action={() => {
+                                                        modalAction();
+                                                        this.setModalVisible();
+                                                    }} />
+                                            </TouchableWithoutFeedback>
+                                        </View>
+                                        : undefined}
+                                </View>
+                                : undefined}
+                            <View style={styles.buttonsBottomContainer}></View>
                         </View>
-                        <View style={styles.contentContainer}>
-                            <Text>
-                                vev
-                                kk
-                                fontFamily: 'ff
-                                f',
-                            </Text>
-                        </View>
-                        <View style={styles.buttonsBottomContainer}></View>
-                    </View>
-                </Modal>
+                    </Modal>
+                </View>
             </View>
         );
     }
 }
 
 ModalBox.protoType = {
-
+    tile: PropTypes.string,
+    hasCloseButton: PropTypes.bool
 };
--- src/components/ModalBox/styles.js
+++ src/components/ModalBox/styles.js
@@ -1,33 +1,84 @@
 import { StyleSheet } from 'react-native';
 
-import { colors, metrics } from '../../styles';
+import { colors, metrics, fonts } from '../../styles';
 
 export default styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
     },
 
     containerModal: {
-        backgroundColor: colors.dark,
-        width: metrics.widthPercentageToDP('80%'),
+        backgroundColor: colors.borderModal,
         borderRadius: 5,
-        padding: 5
+        padding: 5,
+        borderWidth: .5,
+        borderColor: colors.darker,
+        elevation: 5,
+        marginTop: metrics.heightPercentageToDP('25%'),
+        marginHorizontal: 20
     },
 
-    closeButton: {
-        justifyContent: 'flex-end',
-        alignItems: 'flex-end',
-        width: metrics.widthPercentageToDP('8%'),
-        height: metrics.widthPercentageToDP('8%')
-    },
-    
     contentContainer: {
-        backgroundColor: colors.light,
+        backgroundColor: colors.contentModal,
         padding: 5,
         borderRadius: 5,
         width: '100%',
-    }
+        elevation: 2,
+    },
 
+    okButton: {
+        marginTop: 20,
+        alignItems: 'center'
+    },
+
+    textContent: {
+        fontFamily: fonts.fontPrimary,
+        fontSize: fonts.small,
+        color: colors.darker,
+        textAlign: 'center',
+        marginTop: 10,
+        padding: 5
+    },
+
+    closeButtonContainer: {
+        flexDirection: 'row',
+    },
+
+    containerTitle: {
+        flex: 3,
+        alignItems: 'center',
+        textAlign: 'center',
+        marginLeft: 50
+    },
+
+    title: {
+        fontFamily: fonts.fontPrimary,
+        color: colors.white,
+        fontSize: fonts.big,
+    },
+
+    containerCloseButton: {
+        flex: 1,
+        alignSelf: 'flex-end',
+        alignItems: 'flex-end',
+        marginBottom: 5,
+    },
+    
+    closeButton: {
+        width: metrics.widthPercentageToDP('7%'),
+        height: metrics.widthPercentageToDP('7%'),
+    },
+
+    smallFont: {
+        fontSize: fonts.small
+    },
+
+    regularFont: {
+        fontSize: fonts.regular
+    },
+
+    bigFont: {
+        fontSize: fonts.big
+    }
 });
