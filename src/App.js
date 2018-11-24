d9ae1bf4f07f2b40d4326a03b6c29cac02976d7a
Gleydson Rodrigues <gleydsonsr@gmail.com>
2018-11-24 17:24:51 -0300

    update position reducers

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
 import Routes from './Routes';
-import reducers from './reducers';
+import reducers from './store/reducers';
 
 export default class App extends Component {
     render() {
