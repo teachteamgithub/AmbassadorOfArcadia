899c6c4cf06561e52042d441907970e62cf2ba36
Gleydson Rodrigues <gleydsonsr@gmail.com>
2018-11-26 14:29:08 -0300

    final adjustments

--- src/screens/Levels/index.js
+++ src/screens/Levels/index.js
@@ -8,9 +8,12 @@ import {
     AsyncStorage
 } from 'react-native';
 import { Actions } from 'react-native-router-flux';
+import Toast from 'react-native-toast-native';
 
+import Modal from '../../components/ModalBadge';
 import PressButtonAnimationComponent from "../../components/PressButtonAnimationComponent";
 import styles from './styles';
+import { metrics, fonts, colors } from '../../styles';
 
 const BACKGROUND_IMAGE = require('../../assets/images/bg.png');
 
@@ -34,12 +37,22 @@ const levelsImages = {
         require('../../assets/images/levels/icons/level_3_3.png')
     ],
     levelFour: [
-        require('../../assets/images/levels/icons/level_4_0.png'),
-        require('../../assets/images/levels/icons/level_4_1.png'),
-        require('../../assets/images/levels/icons/level_4_2.png'),
         require('../../assets/images/levels/icons/level_4_3.png')
     ]
 };
+
+const styleToast = {
+    width: metrics.widthPercentageToDP('80%'),
+    height: metrics.heightPercentageToDP('20%'),
+    backgroundColor: colors.primary,
+    color: colors.white,
+    borderRadius: 10,
+    fontFamily: fonts.fontPrimary,
+    fontWeight: "bold",
+    lineHeight: 2,
+    lines: 4,
+    fontSize: fonts.big,
+}
 
 export default class LevelsScreen extends Component {
     constructor(props) {
@@ -47,9 +60,13 @@ export default class LevelsScreen extends Component {
         this.state = {
             levelOne: 0,
             levelTwo: 0,
-            levelThree: 0,
-            levelFour: 0
+            isModal: 1,
+            modalVisible: false
         };
+    }
+
+    showToast() {
+        Toast.show('Fase concluida!', Toast.SHORT, Toast.CENTER, styleToast);
     }
 
     componentWillMount() {
@@ -57,17 +74,59 @@ export default class LevelsScreen extends Component {
     }
 
     componentDidUpdate() {
-        AsyncStorage.getItem('levelOne')
-            .then(res => this.state.levelOne !== parseInt(res) ? this.setState({ levelOne: parseInt(res) }) : undefined);
+        this.updateLevelsAndBadges();
+    }
 
-        AsyncStorage.getItem('levelTwo')
-            .then(res => this.state.levelTwo !== parseInt(res) ? this.setState({ levelTwo: parseInt(res) }) : undefined);
+    setModalVisible(value) {
+        this.setState({ modalVisible: !this.state.modalVisible, isModal: value });
+        
+    }
+    async updateLevelsAndBadges() {
 
-        AsyncStorage.getItem('levelThree')
-            .then(res => this.state.levelThree !== parseInt(res) ? this.setState({ levelThree: parseInt(res) }) : undefined);
-
-        AsyncStorage.getItem('levelFour')
-            .then(res => this.state.levelFour !== parseInt(res) ? this.setState({ levelFour: parseInt(res) }) : undefined);
+        let badges = await AsyncStorage.getItem('badges');
+        let levelOne = await AsyncStorage.getItem('levelOne');
+        levelOne = parseInt(levelOne);
+        if (this.state.levelOne === 0 && levelOne === 1) {
+            badges = JSON.parse(badges);
+            badges.badgesOne = 'eneable';
+            await AsyncStorage.setItem('badges', JSON.stringify(badges));
+            await this.setModalVisible(1);
+            await this.setState({ levelOne, isModal: 1 });
+        } else if (this.state.levelOne === 1 && levelOne === 2) {
+            badges = JSON.parse(badges);
+            badges.badgesThree = 'eneable';
+            await AsyncStorage.setItem('badges', JSON.stringify(badges));
+            await this.setModalVisible(3);
+            await this.setState({ levelOne, isModal: 3 });
+        } else if (this.state.levelOne === 2 && levelOne === 3) {
+            badges = JSON.parse(badges);
+            badges.badgesFour = 'eneable';
+            await AsyncStorage.setItem('badges', JSON.stringify(badges));
+            await this.setModalVisible(4);
+            await this.setState({ levelOne, isModal: 4 });
+        }
+        
+        let levelTwo = await AsyncStorage.getItem('levelTwo');
+        levelTwo = parseInt(levelTwo);
+        if (this.state.levelTwo === 0 && levelTwo === 1) {
+            badges = JSON.parse(badges);
+            badges.badgesTwo = 'eneable';
+            await AsyncStorage.setItem('badges', JSON.stringify(badges));
+            await this.setModalVisible(2);
+            await this.setState({ levelTwo, isModal: 2 });
+        } else if (this.state.levelTwo === 1 && levelTwo === 2) {
+            badges = JSON.parse(badges);
+            badges.badgesFive = 'eneable';
+            await AsyncStorage.setItem('badges', JSON.stringify(badges));
+            await this.setModalVisible(5);
+            await this.setState({ levelTwo, isModal: 5 });
+        } else if (this.state.levelTwo === 2 && levelTwo === 3) {
+            badges = JSON.parse(badges);
+            badges.badgesSix = 'eneable';
+            await AsyncStorage.setItem('badges', JSON.stringify(badges));
+            await this.setModalVisible(6);
+            await this.setState({ levelTwo, isModal: 6 });
+        }
     }
 
     configData() {
@@ -77,11 +136,8 @@ export default class LevelsScreen extends Component {
         AsyncStorage.getItem('levelTwo')
             .then(res => this.setState({ levelTwo: parseInt(res) }));
 
-        AsyncStorage.getItem('levelThree')
-            .then(res => this.setState({ levelThree: parseInt(res) }));
-
-        AsyncStorage.getItem('levelFour')
-            .then(res => this.setState({ levelFour: parseInt(res) }));
+        /*AsyncStorage.getItem('levelThree')
+            .then(res => this.setState({ levelThree: parseInt(res) }));*/
     }
 
     getLevelImage(level) {
@@ -102,6 +158,11 @@ export default class LevelsScreen extends Component {
             <ImageBackground
                 source={BACKGROUND_IMAGE}
                 style={styles.backgroundImage}>
+                <Modal
+                    badge={this.state.isModal}
+                    show={this.state.modalVisible}
+                    onClose={() => this.setModalVisible(this.state.isModal)}
+                    title={'Conquistado'}/>
 
                 <ScrollView showsVerticalScrollIndicator={false}>
                     <StatusBar backgroundColor='#1e203d' />
@@ -113,7 +174,7 @@ export default class LevelsScreen extends Component {
                                 image={this.getLevelImage(1)}
                                 width={300 * .6}
                                 height={200 * .6}
-                                actionPress={() => _} />
+                                actionPress={this.state.levelOne === 3 ? this.showToast.bind(this) : Actions.presentation.bind(this)} />
                             <Text style={styles.phaseTextStyle}>Apresentação</Text>
                         </View>
 
@@ -122,26 +183,17 @@ export default class LevelsScreen extends Component {
                                 image={this.getLevelImage(2)}
                                 width={300 * .6}
                                 height={200 * .6}
-                                actionPress={Actions.emotionalRecognition.bind(this)} />
+                                actionPress={this.state.levelTwo === 3 ? this.showToast.bind(this) : Actions.emotionalRecognition.bind(this)} />
                             <Text style={styles.phaseTextStyle}>Reconhecimento de Emoções</Text>
                         </View>
 
                         <View style={styles.levelButton}>
                             <PressButtonAnimationComponent
-                                image={this.getLevelImage(3)}
+                                image={levelsImages.levelThree[0]}
                                 width={300 * .6}
                                 height={200 * .6}
-                                actionPress={() => _} />
-                            <Text style={styles.phaseTextStyle}>Como se comportar</Text>
-                        </View>
-
-                        <View style={styles.levelButton}>
-                            <PressButtonAnimationComponent
-                                image={this.getLevelImage(4)}
-                                width={300 * .6}
-                                height={200 * .6}
-                                actionPress={() => _} />
-                            <Text style={styles.phaseTextStyle}>Reconhecimento de Emoções em grupo</Text>
+                                actionPress={Actions.erg.bind(this)} />
+                            <Text style={styles.phaseTextStyle}>Jogar em grupo</Text>
                         </View>
 
                     </View>
